e7cd928558daaecf745e83ae129d1c08a1b38c52
Francisco-Ivanir <ficfivanir@outlook.com>
2026-06-28 12:06:37 -0300

    Update script.js

--- script.js
+++ script.js
@@ -63,7 +63,21 @@ alert("onValue = " + typeof window.onValue);
   const raffleRef = window.ref(window.db, "raffleData");
 
   alert("V2.1 - referência criada");
+   
+window.get(raffleRef)
+  .then((snapshot) => {
+    alert("GET FUNCIONOU");
 
+    if (snapshot.exists()) {
+      alert("DADOS EXISTEM");
+    } else {
+      alert("SEM DADOS");
+    }
+  })
+  .catch((err) => {
+    alert("ERRO GET: " + err.message);
+  });
+   
   window.onValue(raffleRef, (snapshot) => {
 
     alert("V2.1 - onValue disparou");
